c52dd486c2ac88e242c0b29552c34200091360dd
cas <cas@interglobal.vision>
2017-07-06 19:09:26 -0500

    fix functions

--- functions.php
+++ functions.php
@@ -43,7 +43,7 @@ get_template_part( 'lib/post-types' );
 get_template_part( 'lib/meta-boxes' );
 get_template_part( 'lib/theme-options' );
 
-add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 9999 );
+add_action( 'init', 'cmb_initialize_cmb_meta_boxes', 11 );
 function cmb_initialize_cmb_meta_boxes() {
   // Add CMB2 plugin
   if( ! class_exists( 'cmb2_bootstrap_202' ) )
